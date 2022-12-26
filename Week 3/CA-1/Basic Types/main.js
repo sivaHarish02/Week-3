b94efd044d4076a92923077093ad99214be36edd
sivaHarish02 <118168273+sivaHarish02@users.noreply.github.com>
2022-12-26 20:57:41 +0530

    Add files via upload

---
+++ Week 3/CA-1/Basic Types/main.js
@@ -0,0 +1,20 @@
+/*
+Basic Types activity
+- Create 3 variables and set them up to the given instructions. Also, type check them with the typeof operator in the console.
+*/
+//your code here
+var myFirstName ="myFirstName";
+var numberOfCities= 3;
+var havingFun=true;
+
+
+///////////////////////////////////
+//check results in browser console
+console.log('myFirstName: ', typeof myFirstName);
+console.log('numberOfCities: ', typeof numberOfCities);
+console.log('havingFun: ', typeof havingFun);
+
+// Do not edit code past this point
+if (typeof module !== 'undefined') {
+  module.exports = { myFirstName, numberOfCities, havingFun };
+}
